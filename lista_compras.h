7ea58e3cb338e0d614232399eda6b72d956ea794
ju-caju <julianajfb.barreto@gmail.com>
2026-07-16 13:29:00 -0300

    feat(lista-compras): definir estruturas do historico de compras

---
+++ lista_compras.h
@@ -0,0 +1,21 @@
+#ifndef LISTA_COMPRAS_H
+#define LISTA_COMPRAS_H
+
+#include <list>
+#include <map>
+#include <string>
+#include <vector>
+
+struct ListaCompras {
+    std::vector<std::string> codigos_clientes;
+    std::map<std::string, int> indices_clientes;
+    std::vector<std::string> nomes_produtos;
+    std::map<std::string, int> indices_produtos;
+    std::vector<std::list<int> > compras_clientes;
+};
+
+bool carregarListaCompras(ListaCompras &lista, const char *caminho_arquivo);
+bool obterIndiceCliente(const ListaCompras &lista, const std::string &codigo_cliente, int &indice_cliente);
+void exibirComprasCliente(const ListaCompras &lista, const std::string &codigo_cliente);
+
+#endif
