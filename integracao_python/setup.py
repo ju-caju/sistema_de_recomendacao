c12100589e7a2043db0f1d85c74924407089b714
ju-caju <julianajfb.barreto@gmail.com>
2026-08-02 16:52:21 -0300

    build(python): configurar extensao pybind11 em C++11

---
+++ integracao_python/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+
+
+ext_modules = [
+    Pybind11Extension(
+        "sistema_recomendacao",
+        [
+            "bindings.cpp",
+            "../similaridade.cpp",
+            "../recomendacao.cpp",
+            "../recomendacao_csr.cpp",
+            "../csr.cpp",
+        ],
+        include_dirs=[".."],
+        cxx_std=11,
+    )
+]
+
+
+setup(
+    name="sistema_recomendacao",
+    version="0.1.0",
+    description="Integracao Python-C++ do sistema de recomendacao",
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
+)
